dfc8225637cc2efb3b968d751438e875bdee43dc
ezfira <Citrix6060>
2016-08-23 12:29:40 +0300

    Bot provides multiple options for question

--- scripts/stack-search.js
+++ scripts/stack-search.js
@@ -1,25 +1,27 @@
 var stackexchange = require('stackexchange')
 var Entities = require('html-entities').AllHtmlEntities
 
+var entities = new Entities();
+const options = { version: 2.2 }
+let context = new stackexchange(options)
+
+let searchQuery = {
+  site: 'stackoverflow',
+  sort: 'relevance',
+  order: 'desc',
+  accepted: true,
+  filter: '!9YdnS9cov',
+  pagesize: 10
+}
+
+let questionQuery = {
+  site: 'stackoverflow',
+  sort: 'activity',
+  order: 'desc',
+  filter: '!9YdnSM68f'
+}
+
 module.exports = function(robot) {
-
-  let entities = new Entities();
-  let options = { version: 2.2 }
-  let context = new stackexchange(options)
-
-  let searchQuery = {
-    site: 'stackoverflow',
-    sort: 'relevance',
-    order: 'desc',
-    accepted: true
-  }
-
-  let questionQuery = {
-    site: 'stackoverflow',
-    sort: 'activity',
-    order: 'desc',
-    filter: '!9YdnSM68f'
-  }
 
   robot.respond(/debug/, function(msg) {
 
@@ -34,6 +36,41 @@ module.exports = function(robot) {
     // Extract the actual question
     let botNameRegEx = new RegExp("@*" + msg.robot.name + ":*")
     let msgTxt = msg.message.text.replace(botNameRegEx, '').trim()
+
+    let userId = msg.message.user.id
+
+    // Check if user already asked a question
+    let userData = robot.brain.get(userId)
+    if (userData) {
+      let msgIdx = parseInt(msgTxt)
+
+      // If not a number assume its a new question
+      if (!isNaN(msgIdx)) {
+
+        // Check that the user provided a valid question number
+        if (msgIdx < 1 || msgIdx > userData.questions.length) {
+          msg.reply('Didn\'t get that.. here are the options again:\n' + formatPossibleQuestions(userData.questions))
+          return
+        }
+
+        // Answer question
+        answerQuestion(userData.questions[msgIdx - 1].question_id, userData.questions[msgIdx - 1].title, msg)
+
+        // Clear user question
+        robot.brain.remove(userId)
+
+        return
+      }
+    }
+
+    // Clear user question
+    robot.brain.remove(userId)
+
+    // If message is just one word don't run the search
+    if (msgTxt.split(' ').length === 1) {
+      msg.reply('Not quite sure what you expect me to do with *' + msgTxt + '*')
+      return
+    }
 
     searchQuery.q = msgTxt
 
@@ -50,33 +87,56 @@ module.exports = function(robot) {
         return
       }
 
-      // Get the most relevant question
-      let relevantQuestion = response.items[0];
+      if (response.items.length === 1) {
+        answerQuestion(response.items[0].question_id, response.items[0].title, msg)
+        return
+      }
 
-      msg.send('Here is what I know about *' + relevantQuestion.title + "*")
+      // Get the top 3 questions
+      let questions = response.items.sort((q1, q2) => {
+        return q2.up_vote_count - q1.up_vote_count
+      }).slice(0, 3);
 
-      // Get answers for relevant question
-      context.questions.answers(questionQuery, function(err, response) {
-        if (err) {
-          console.error('Answer error', err);
-          msg.send('Oh oh.. something went wrong with answers', err)
-          return
-        }
+      // Set the user questions
+      robot.brain.set(userId, {questions: questions})
 
-        // Find the accepted answer
-        let acceptedAnswer = response.items.find(function(answer) {
-          return answer.is_accepted;
-        })
-
-        // Reply with accepted answer
-        if (acceptedAnswer) {
-          msg.send(entities.decode(acceptedAnswer.body_markdown))
-          return
-        }
-
-        msg.send('Hmmm.. couldn\'t find an answer')
-
-      }, [relevantQuestion.question_id])
+      msg.reply('I have several possible answers. Which describes best your question?\n' + formatPossibleQuestions(questions) + '\n\nIf none of the above fits, you need to refine your question')
     })
   });
+}
+
+function answerQuestion(qId, qTitle, msg) {
+
+  msg.send('Here is what I know about *' + qTitle + "*")
+  context.questions.answers(questionQuery, function(err, response) {
+    if (err) {
+      console.error('Answer error', err);
+      msg.send('Oh oh.. something went wrong with answers', err)
+      return
+    }
+
+    // Find the accepted answer
+    let acceptedAnswer = response.items.find(function(answer) {
+      return answer.is_accepted;
+    })
+
+    // Reply with accepted answer
+    if (acceptedAnswer) {
+      msg.send(entities.decode(acceptedAnswer.body_markdown))
+      return
+    }
+
+    msg.send('Hmmm.. couldn\'t find an answer')
+
+  }, [qId])
+}
+
+function formatPossibleQuestions(questions) {
+  let str = ""
+
+  questions.forEach((q, idx) => {
+    str += (idx + 1) + ") *" + q.title + "*\n"
+  })
+
+  return str
 }
